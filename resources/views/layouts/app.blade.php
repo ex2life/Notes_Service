75f805bd02218f67b4abe98936b2660fcfceabb1
sergey <abramizsaransk@gmail.com>
2018-12-25 18:10:55 +0300

    Фон страниц

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>Заметки</title>
+    <style>
+        body {
+            background-image: url(https://cdn1.savepice.ru/uploads/2018/12/25/a59306a5a539d9278212b197e4f0b56c-full.jpg); /* Путь к фоновому изображению */
+        }
+    </style>
 </head>
 <body>
     <div class="container">
